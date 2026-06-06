9dfc417de1afee7c4dc55613389b9960ce2ca187
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-06-06 10:07:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -632,7 +632,7 @@ return (
           {entry.count} Eier
           <br />
           <small>
-  {new Date(entry.date).toLocaleDateString('de-DE')}
+  {new Date(entry.vaccination_date).toLocaleDateString('de-DE')}
 </small>
         </div>
 
